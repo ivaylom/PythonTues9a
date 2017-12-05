15ea32bef4070a488a8579afd3f5e708c80e681b
Ivaylo Mihaylov <ivaylo.mihaylov@IvayloMacBook.local>
2017-12-05 08:21:17 +0200

    graphic

--- _2048.py
+++ _2048.py
@@ -1,14 +1,23 @@
-"""from tkinter import *
+import _2048engine
+
+from tkinter import *
 t = Tk()
 
 def callback(e):
-    print(e)
+    if e.keysym == 'Up':
+        print(e)
+    elif e.keysym == 'Down':
+        print(e)
 
 t.bind("<Up>", callback)
-t.mainloop()
-5"""
 
-import _2048engine
+c = Canvas(t, width=400, height=400)
+c.pack()
+
+c.create_rectangle(0,0,40,40, fill="red", outline="red")
+c.create_text(20,20, text="2048")
+
+t.mainloop()
 
 engine = _2048engine._2048()
 engine.addItem()
