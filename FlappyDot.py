ae5d5265dcc91b0563b4220c6851098648814d56
ivaylo <ivaylo.mihaylov@botronsoft.com>
2018-03-07 18:27:23 +0200

    Collision

--- FlappyDot.py
+++ FlappyDot.py
@@ -23,12 +23,37 @@ def callback(e):
     elif e.keysym == "Down" and c.coords(dot)[1] < maxY:
         c.move(dot, 0, 10)
 
+def isCollideObj(a, b):
+    aPos = c.coords(a)
+    bPos = c.coords(b)
+    isCol = (isCollideOneAxis(aPos[0], aPos[2], bPos[0], bPos[2]) or \
+           isCollideOneAxis(bPos[0], bPos[2], aPos[0], aPos[2])) and \
+           (isCollideOneAxis(aPos[1], aPos[3], bPos[1], bPos[3]) or \
+           isCollideOneAxis(bPos[1], bPos[3], aPos[1], aPos[3]))
+    if isCol:
+        c.itemconfig(b, fill="red")
+    else:
+        c.itemconfig(b, fill="green")
+
+def isCollideOneAxis(ax1, ax2, bx1, bx2):
+    return ax1 < bx2 and ax2 > bx2 or \
+           ax2 > bx1 and ax1 < bx1
+
 def createPipe():
     holeY = randint(0, height-holeSize)
-    first = c.create_rectangle(width+10, 0, width+10+pipeWidth, holeY, fill="red")
-    second = c.create_rectangle(width+10, holeY+holeSize, width+10+pipeWidth, height, fill="red")
+    first = c.create_rectangle(width+10, 0, width+10+pipeWidth, holeY, fill="green")
+    second = c.create_rectangle(width+10, holeY+holeSize, width+10+pipeWidth, height, fill="green")
     return (first, second)
 pipes = []
+
+def isCollide():
+    global pipes
+    global dot
+    for p in pipes:
+        for rect in p:
+            if isCollideObj(dot, rect):
+                return True
+    return False
 
 def movePipe(pipe):
     global pipes
@@ -52,6 +77,10 @@ def onTimer():
     if (counter > 20):
         pipes.append(createPipe())
         counter = 0
+    if isCollide():
+        print("Collide")
+    else:
+        print("-")
 
 t.bind("<Up>", callback)
 t.bind("<Down>", callback)
